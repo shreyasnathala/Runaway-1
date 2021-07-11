c98dec5c4f4d7a28525b4cb91e7599c3578ca155
shreyasnathala <70395653+shreyasnathala@users.noreply.github.com>
2021-07-11 13:50:22 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var Bob,Bobimg
+var bgimg
+function preload() {
+  bgimg=loadImage("images/Game Background.jpg")
+}
+
+function setup(){
+createCanvas(1000,800)
+}
+
+function draw() {
+  background(bgimg)
+}
